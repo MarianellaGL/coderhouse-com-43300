2aa0702d5eaf82158efbe6cebe1b073022bcb594
marianellagl <marianella.gomezluna@gmail.com>
2023-06-21 18:35:25 -0300

    clase 01

---
+++ clase1/clase01/clase01/clase01/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <button onClick={() => console.log("click")}>Hola!</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
